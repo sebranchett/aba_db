3a7efa1d42187cb7a840e08dabec13baf052ad8b
sebranchett <s.e.branchett@tudelft.nl>
2026-06-01 16:58:33 +0200

    read all versions of SPOORTAK from a folder

--- fill_spoortak.m
+++ fill_spoortak.m
@@ -25,76 +25,87 @@ else
     fprintf('Connection failed: %s\n', conn.Message);
 end
 
-load("<folder_name>\SPOORTAK_25.mat");
-T = Spoortakken.AllGeometry;     % table
-n = height(T);
-disp(n);
+folder = fullfile(pwd, 'SpoortakkenCollectie');
+fprintf('folder is: %s\n', folder);
+files = dir(fullfile(folder, 'SPOORTAK_*.mat'));
 
-version = 'SPOORTAK_25';
+for k = 1:numel(files)
+    fname = files(k).name;                          % filename only
+    fpath = fullfile(folder, fname);                % full path to file
 
-startIndex = 1;
-endIndex = n;
-oldSpoortak = T(1, 3);
-pts = '';
-for i = startIndex:endIndex
-    spoortak = T(i, 3);
-    if spoortak ~= oldSpoortak;
-        spoortakName = sprintf('Spoortak %d', oldSpoortak);
-        pts(1) = [];  % Remove first space
-        pts(end) = [];  % Remove the last comma
-        wkt = sprintf('LINESTRING(%s)', pts);
-        % disp(wkt);
+    txt = fileread(fpath);
 
-        % Build SQL (use sprintf to insert numeric values into the WKT)
-        sql = sprintf([
-            'INSERT INTO spoortak_traces(spoortak_version, spoortak_name, geom) ' ...
-            'VALUES (''%s'', ''%s'', ST_GeomFromText(''%s'', 4326))'], ...
-            version, spoortakName, wkt);
-        % disp(sql);
-        
-        % Execute
-        curs = exec(conn, sql);
-        % Check for errors on cursor
-        if isprop(curs, 'Message') && ~isempty(curs.Message)
-            error('SQL execution failed: %s', curs.Message);
+    load(fpath);
+    T = Spoortakken.AllGeometry;     % table
+    n = height(T);
+    disp(n);
+    
+    [~, version, ~] = fileparts(fname);  % strip of the .mat part
+    
+    startIndex = 1;
+    endIndex = n;
+    oldSpoortak = T(1, 3);
+    pts = '';
+    for i = startIndex:endIndex
+        spoortak = T(i, 3);
+        if spoortak ~= oldSpoortak
+            spoortakName = sprintf('Spoortak %d', oldSpoortak);
+            pts(1) = [];  % Remove first space
+            pts(end) = [];  % Remove the last comma
+            wkt = sprintf('LINESTRING(%s)', pts);
+            % disp(wkt);
+    
+            % Build SQL (use sprintf to insert numeric values into the WKT)
+            sql = sprintf([
+                'INSERT INTO spoortak_traces(spoortak_version, spoortak_name, geom) ' ...
+                'VALUES (''%s'', ''%s'', ST_GeomFromText(''%s'', 4326))'], ...
+                version, spoortakName, wkt);
+            % disp(sql);
+            
+            % Execute
+            curs = exec(conn, sql);
+            % Check for errors on cursor
+            if isprop(curs, 'Message') && ~isempty(curs.Message)
+                error('SQL execution failed: %s', curs.Message);
+            end
+            % Close cursor
+            close(curs);
+
+            % Commit transaction (if necessary)
+            % Many drivers autocommit; calling commit is safe when available.
+            try
+                commit(conn);
+            catch
+                % Some connections don't support commit (autocommit); ignore if fails
+            end
+    
+            oldSpoortak = spoortak;
+            pts = '';
         end
-        % Close cursor
-        close(curs);
-
-        % Commit transaction (if necessary)
-        % Many drivers autocommit; calling commit is safe when available.
-        try
-            commit(conn);
-        catch
-            % Some connections don't support commit (autocommit); ignore if fails
-        end
-
-        oldSpoortak = spoortak;
-        pts = '';
+        pts = sprintf('%s %0.8f %0.8f,', pts, T(i, 1), T(i, 2) );
     end
-    pts = sprintf('%s %0.8f %0.8f,', pts, T(i, 1), T(i, 2) );
+    spoortakName = sprintf('Spoortak %d', oldSpoortak);
+    pts(1) = [];  % Remove first space
+    pts(end) = [];  % Remove the last comma
+    wkt = sprintf('LINESTRING(%s)', pts);
+    % disp(wkt);
+    
+    % Build SQL (use sprintf to insert numeric values into the WKT)
+    sql = sprintf([
+        'INSERT INTO spoortak_traces(spoortak_version, spoortak_name, geom) ' ...
+        'VALUES (''%s'', ''%s'', ST_GeomFromText(''%s'', 4326))'], ...
+        version, spoortakName, wkt);
+    % disp(sql);
+    
+    % Execute
+    curs = exec(conn, sql);
+    % Check for errors on cursor
+    if isprop(curs, 'Message') && ~isempty(curs.Message)
+        error('SQL execution failed: %s', curs.Message);
+    end
+    % Close cursor
+    close(curs);
 end
-spoortakName = sprintf('Spoortak %d', oldSpoortak);
-pts(1) = [];  % Remove first space
-pts(end) = [];  % Remove the last comma
-wkt = sprintf('LINESTRING(%s)', pts);
-% disp(wkt);
-
-% Build SQL (use sprintf to insert numeric values into the WKT)
-sql = sprintf([
-    'INSERT INTO spoortak_traces(spoortak_version, spoortak_name, geom) ' ...
-    'VALUES (''%s'', ''%s'', ST_GeomFromText(''%s'', 4326))'], ...
-    version, spoortakName, wkt);
-% disp(sql);
-
-% Execute
-curs = exec(conn, sql);
-% Check for errors on cursor
-if isprop(curs, 'Message') && ~isempty(curs.Message)
-    error('SQL execution failed: %s', curs.Message);
-end
-% Close cursor
-close(curs);
 
 % Commit transaction (if necessary)
 % Many drivers autocommit; calling commit is safe when available.
